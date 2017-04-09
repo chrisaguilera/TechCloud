d58f34dc28fcba6fac02547ed49bdd74d01b2e8f
Srivas Tirumala <stirumal@usc.edu>
2017-04-09 14:07:32 -0700

    php file for the index and css file. Radio buttons work as expected

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>TechCloud Home</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<!-- <script src="APIHandler.js"></script> -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/index.css">
+	<link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+<body>
+	<h1 id="mainHeader">Welcome to TechCloud</h1>
+	<div class="container">
+		<div class="form-group">
+			<input class="form-control" id="input-text" list="previoussearchlist" aria-describedby="emailHelp" placeholder="Enter Artist" autocomplete="off"></input>
+			<datalist id="previoussearchlist"></datalist>
+		</div>
+
+		<div class="radio">
+			<form>
+			    <label class="radio-inline">
+			      <input type="radio" name="optradio" id="nameRadio" checked="true">Search by Name
+			    </label>
+			    <label class="radio-inline">
+			      <input type="radio" name="optradio" id="keywordRadio">Search by Keyword Phrase
+			    </label>
+			</form>
+		</div>
+		<button class="search-button btn pull-left" type="search"> Search </button>
+	</div>
+
+	<script type="text/javascript">
+		// function checkRadio(){
+		// 	if (document.getElementById('nameRadio').checked){
+		// 		console.log('name is checked');
+		// 	} else if (document.getElementById('keywordRadio')){
+		// 		console.log('keywordRadio');
+		// 	}
+		// }
+
+		$(document).ready(function(){
+			
+		});
+	</script>
+</body>
+</html>
