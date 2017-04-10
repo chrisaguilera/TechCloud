17183f02d5ae0910a440a62f2edbba2f31eb4eb4
Emanuel Azage <Emanuel_Azage@Emanuels-MacBook-Pro-3.local>
2017-04-09 17:22:32 -0700

    able to do do searches on IEEE api w/ Nick

--- IEEEHandler.js
+++ IEEEHandler.js
@@ -1,4 +1,21 @@
 
-function test(input){
-	
+function printResultsForAuthor(author){
+	var pdfURL;
+	$.ajax({
+	    url: "http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?au="+author,
+	    // url: "http://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=7515472",
+	    dataType: "xml",
+	    success: function( response ) {
+	      // console.log( response ); // server response
+		  pdfURL = response.getElementsByTagName("document")[0].getElementsByTagName("pdf")[0]["textContent"];
+		  console.log(pdfURL);
+		  $.ajax({
+		  	url: pdfURL,
+		  	dataType: "xml",
+		  	success: function(pdf){
+		  		console.log(pdf);
+		  	}
+		  });
+	    }
+  	});
 }
