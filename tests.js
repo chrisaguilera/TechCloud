f7003a186f143fdf237398b9d8759933880ca280
Emanuel Azage <eazage@usc.edu>
2017-04-17 00:16:01 -0700

    added tests for IEEE api calls

--- tests.js
+++ tests.js
@@ -50,18 +50,9 @@ $(document).ready(function () {
     //runTest("jus", ["Justin Bieber","Justin Timberlake","Justin Moore","Justin Quiles","Justice","Justine Skye","Justin Nozuka","Justice Crew","Justin Hurwitz"]);
 
     QUnit.test( "Test getAbstractForDocTitle()", function( assert ) {
-      assert.expect(0);
+          assert.expect(1);
       var correctAbstract = "Web applications can be easily made available to an international audience by leveraging frameworks and tools for automatic translation and localization. However, these automated changes can distort the appearance of web applications since it is challenging for developers to design their websites to accommodate the expansion and contraction of text after it is translated to another language. Existing web testing techniques do not support developers in checking for these types of problems and manually checking every page in every language can be a labor intensive and error prone task. To address this problem, we introduce an automated technique for detecting when a web page's appearance has been distorted due to internationalization efforts and identifying the HTML elements or text responsible for the observed problem. In evaluation, our approach was able to detect internationalization problems in a set of 54 web applications with high precision and recall and was able to accurately identify the underlying elements in the web pages that led to the observed problem."
       var abstract;
-      // var done = assert.async();
-      //   function callback(returnVal) {
-      //     abstract = returnVal;
-      //   }
-      //   setTimeout(function(){
-      //     assert.deepEqual(abstract, correctAbstract);
-      //     done();
-      //   }, 100);
-
 
       function getAbstractForDocTitle(title){
         $.ajax({
@@ -77,6 +68,44 @@ $(document).ready(function () {
       var input = "Detecting and Localizing Internationalization Presentation Failures in Web Applications"
       getAbstractForDocTitle(input);
 
+      var done = assert.async();
+          setTimeout(function(){
+            assert.deepEqual(abstract, correctAbstract);
+            console.log("after timeout: " + abstract);
+            done();
+          }, 2000);
+
     });
 
-  });
+    QUnit.test( "Test Getting papers for keywords", function( assert ) {
+          assert.expect(1);
+          var correctAbstract = "Web applications can be easily made available to an international audience by leveraging frameworks and tools for automatic translation and localization. However, these automated changes can distort the appearance of web applications since it is challenging for developers to design their websites to accommodate the expansion and contraction of text after it is translated to another language. Existing web testing techniques do not support developers in checking for these types of problems and manually checking every page in every language can be a labor intensive and error prone task. To address this problem, we introduce an automated technique for detecting when a web page's appearance has been distorted due to internationalization efforts and identifying the HTML elements or text responsible for the observed problem. In evaluation, our approach was able to detect internationalization problems in a set of 54 web applications with high precision and recall and was able to accurately identify the underlying elements in the web pages that led to the observed problem."
+          var abstract; 
+          // function callback(returnVal) {
+          //   abstract = returnVal;
+          // }
+
+          function getPapersForWords(search){
+          $.ajax({
+            url: "http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?querytext="+search,
+            dataType: "xml",
+            success: function( response ) {
+              abstract = response.getElementsByTagName("document")[0].getElementsByTagName("abstract")[0]["textContent"];
+              console.log(abstract);
+            }
+          });
+        }
+
+          var input = "Detecting and Localizing Internationalization Presentation Failures in Web Applications";
+          getPapersForWords(input);
+
+          var done = assert.async();
+          setTimeout(function(){
+            assert.deepEqual(abstract, correctAbstract);
+            console.log("after timeout: " + abstract);
+            done();
+          }, 2000);
+          // assert.deepEqual(abstract, correctAbstract);
+        });
+
+      });
