3cd6e6cd1259b529afa47095b75d6b9f6cb8bb6d
cogschris <cognettachris@gmail.com>
2017-04-27 11:54:51 -0700

    Added more php files

---
+++ GetSubset.php
@@ -0,0 +1,17 @@
+<?php
+// Start the session
+session_start();
+
+if(!isset($_SESSION['prevSearches'])) {
+	$_SESSION['prevSearches'] = [];
+}
+if(!isset($_SESSION['authors'])) {
+	$_SESSION['authors'] = [];
+}
+if(!isset($_SESSION['keywords'])) {
+	$_SESSION['keywords'] = [];
+}
+
+echo json_encode($_SESSION['subsets']);
+
+?>
