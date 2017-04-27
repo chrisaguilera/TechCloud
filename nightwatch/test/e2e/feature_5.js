3643c84720c26c3b4e373a273a9de23cf3c1befb
ShainaG <shainagill@Shainas-MacBook-Pro.local>
2017-04-26 21:25:39 -0700

    blackbox test for feature 5 w/ Chris A

---
+++ nightwatch/test/e2e/feature_5.js
@@ -0,0 +1,20 @@
+var conf = require('../../nightwatch.conf.js');
+
+module.exports = {
+  'Feature 5 Test': function (browser) {
+    browser
+      .url('http://localhost:8888/index.php')
+      .waitForElementVisible('body')
+      .setValue('input[id="input-text"]', 'Halfond')
+      .click('input[id="nameRadio"]')
+      .click('button[type="search"]')
+      .waitForElementVisible('p[id="wordcloudparagraph"]')
+      .click('span[id="web"]')
+      .waitForElementVisible ('div[class="paperTable"]')
+      .click ('td[id="10.1109/ISSRE.2012.37"]');
+
+    browser
+      .assert.title('List of Papers')
+      .end();
+  }
+};
