94d39fdad8592b8c8a2a8a9a77ba87541029abba
Your Name Here <agui150@usc.edu>
2017-04-28 00:30:41 -0700

    refactoring

--- nightwatch/test/e2e/feature_7.js
+++ nightwatch/test/e2e/feature_7.js
@@ -1,9 +1,9 @@
 var conf = require('../../nightwatch.conf.js');
 
 module.exports = {
-  'Feature 11 Test': function (browser) {
+  'Feature 7 Test': function (browser) {
     browser
-      .url('http://localhost:8880/index.php')
+      .url('http://localhost:8888/index.php')
       .waitForElementVisible('body')
       .setValue('input[id="input-text"]', 'Halfond')
       .click('input[id="nameRadio"]')
@@ -12,7 +12,7 @@ module.exports = {
       .setValue('input[id="input-text"]', 'Boehm')
       .click('input[id="nameRadio"]')
       .click('button[type="search"]')
-      .waitForElementVisible('p[id="wordcloudparagraph"]');
+      .waitForElementVisible('span[id="systems"]');
 
     browser
       .assert.title('Boehm')
