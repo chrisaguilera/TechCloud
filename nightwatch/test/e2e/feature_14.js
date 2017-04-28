bf0f84654dbbd398179b26343f9db8310a72cf4c
ShainaG <shainagill@Shainas-MacBook-Pro.local>
2017-04-28 03:33:18 -0700

    black box test for feature 14

---
+++ nightwatch/test/e2e/feature_14.js
@@ -0,0 +1,18 @@
+var conf = require('../../nightwatch.conf.js');
+
+module.exports = {
+  'Feature 14 Test': function (browser) {
+    browser
+      .url('http://localhost:8888/index.php')
+      .waitForElementVisible('body')
+      .setValue('input[id="input-text"]', 'parallel computing')
+      .click('input[id="keywordRadio"]')
+      .click('button[type="search"]')
+      .waitForElementVisible('p[id="wordcloudparagraph"]')
+
+    browser
+      .assert.title('Abstract Page')
+      .assert.elementPresent ('p[id="wordcloudparagraph"]')
+      .end();
+  }
+};
