6c755c507a7f72f2a97e8cfa833d0fb56e1bec08
Your Name Here <agui150@usc.edu>
2017-04-28 00:31:03 -0700

    merge

--- nightwatch/test/e2e/feature_8.js
+++ nightwatch/test/e2e/feature_8.js
@@ -13,10 +13,19 @@ module.exports = {
       .waitForElementVisible ('div[class="paperTable"]')
       .click ('span[id="2012 IEEE 23rd International Symposium on Software Reliability Engineering"]')
       .waitForElementVisible ('div[class="paperTable"]')
-      .attributeEquals ("2015 IEEE 8th International Conference on Software Testing, Verification and Validation (ICST)");
 
     browser
       .assert.title('List of Papers')
+      .assert.elementPresent ('td[id="Automated Checking of Web Application Invocations"]')
+      .assert.elementPresent ('td[id="Data Loss Prevention Based on Data-Driven Usage Control"]')
+      .assert.elementPresent ('td[id="Dynamic Analysis of Upgrades in C/C++ Software"]')
+      .assert.elementPresent ('td[id="Improving Coverage-Based Localization of Multiple Faults Using Algorithms from Integer Linear Programming"]')
+      .assert.elementPresent ('td[id="Lightweight Automated Testing with Adaptation-Based Programming"]')
+      .assert.elementPresent ('td[id="MoTiP 2012 Workshop Organizers"]')
+      .assert.elementPresent ('td[id="Software Life-Extension: A New Countermeasure to Software Aging"]')
+      .assert.elementPresent ('td[id="Speculative Symbolic Execution"]')
+      .assert.elementPresent ('td[id="Using Multiclass Machine Learning Methods to Classify Malicious Behaviors Aimed at Web Systems"]')
+      .assert.elementPresent ('td[id="What Is System Hang and How to Handle It"]')
       .end();
   }
 };
